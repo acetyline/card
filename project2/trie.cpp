5a8b30fc403ab13d56edd7afb136f0c5f31c5ee9
LHRG <78004319+LHRG@users.noreply.github.com>
2021-01-27 11:01:42 +0800

    Update trie.cpp

--- project2/trie.cpp
+++ project2/trie.cpp
@@ -5,9 +5,9 @@ using namespace std;
 class trie
 {
 public:
-	//tree：Trie 树 
+	//tree：Trie 树。
     map<int,map<int,int> > tree;
-    //num：某个玩家所在的位置，arcnum：某个位置的玩家 
+    //num：某个玩家所在的位置，arcnum：某个位置的玩家。
     map<int,int> num,arcnum;
     int cnt=0;
     void clear()
@@ -17,7 +17,7 @@ public:
     	arcnum.clear();
     	cnt=0; 
 	}
-    //player:某个玩家的操作 card:加入的基本牌
+    //player:某个玩家的操作，card:加入的基本牌。
     void add(int player,int card)
 	{
 		int k=0;
