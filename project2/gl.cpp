e1a1a961d6177c6c97ea43c346761b34d074d160
unknown <2321759131@qq.com>
2021-02-01 18:03:29 +0800

    fadsfdsafdsafdsafdsafdsaf

--- project2/gl.cpp
+++ project2/gl.cpp
@@ -22,12 +22,11 @@ float r;
 void update(trie a)
 {
     t = a.putout();
-    layer = t.size();
 }
 
-float r = 0.04;
-//上下行间距为一个半径，最上面最下面留两个半径
-
+float r = 0.02;
+//上下行间距为2r，最上面最下面留4r
+//pts.x=4r+(layer-1)*2r+r
 point center;
 int a;
 
@@ -50,8 +49,8 @@ int main(int argc,char ** argv)
     glClearColor(1.0f, 1.0f, 1.0f);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
-    glutInitWindowSize(600, 600);
-    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(800, 800);
+    glutInitWindowPosition(50, 50);
     glutCreateWindow("test");
     for(auto i:pts)
     {
